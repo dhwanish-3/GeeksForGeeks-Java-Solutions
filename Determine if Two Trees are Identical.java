d7e8e62804afe3a08691b703547cc80d132bcf82
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-11-22 22:38:51 +0530

    Easy why

---
+++ Determine if Two Trees are Identical.java
@@ -0,0 +1,26 @@
+class Node{
+    int data;
+    Node left, right;
+    Node(int d){
+        data = d;
+        left = right = null;
+    }
+}
+
+class Solution
+{
+    //Function to check if two trees are identical.
+	boolean isIdentical(Node root1, Node root2)
+	{
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.data != root2.data) {
+            return false;
+        }
+        return isIdentical(root1.left, root2.left) && isIdentical(root1.right, root2.right);
+	}
+}
---
+++ Symmetric Tree.java
@@ -0,0 +1,31 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG
+{
+    public static boolean isSymmetric(Node left, Node right) {
+        if(left == null && right == null) {
+            return true;
+        }
+        if(left != null && right != null && left.data == right.data) {
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+        return false;
+    }
+    // return true/false denoting whether the tree is Symmetric or not
+    public static boolean isSymmetric(Node root)
+    {
+        if(root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+}
