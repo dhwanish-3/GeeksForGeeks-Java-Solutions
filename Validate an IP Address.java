3919fe049584f40b0d78f0d15d20a69c15dadb7a
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2024-08-10 13:59:12 +0530

    Ip addr

---
+++ Validate an IP Address.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String str) {
+        // Write your code here
+        String str2 = str.replace("."," ");
+        String[] strarr = str2.split(" ");
+        if (strarr.length != 4) return false;
+        for (String s : strarr){
+            if (s.trim().equals("")) return false;
+            if (s.length() > 1 && s.startsWith("0")) return false;
+            int a = Integer.parseInt(s);
+            if (a < 0 || a > 255) return false;
+        }
+        return true;
+    }
+}
